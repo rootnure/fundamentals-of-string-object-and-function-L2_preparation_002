a0fcbdbc9ab0e82b3c3867e84ad825e4018cbffe
rootnure <nur.diu.2791@gmail.com>
2024-08-30 21:32:46 +0600

    000

---
+++ string-operations.js
@@ -0,0 +1,16 @@
+const sentence = "I am learning web development.";
+// const result = ".tnempoleved bew gninrael ma I";
+
+let reverse = '';
+
+// for(const letter of sentence) {
+//     // console.log(letter);
+//     reverse = letter + reverse;
+// }
+// console.log(reverse);
+
+// for(let i = 0; i < sentence.length; i++) {
+//     // console.log(sentence[i]);
+//     reverse = sentence[i] + reverse;
+// }
+// console.log(reverse);
