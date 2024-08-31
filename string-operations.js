4f44466052bed02cf0a06c537be8f60ccfdc9bdc
rootnure <nur.diu.2791@gmail.com>
2024-08-31 23:35:15 +0600

    001

--- string-operations.js
+++ string-operations.js
@@ -9,8 +9,8 @@ let reverse = '';
 // }
 // console.log(reverse);
 
-// for(let i = 0; i < sentence.length; i++) {
-//     // console.log(sentence[i]);
-//     reverse = sentence[i] + reverse;
-// }
-// console.log(reverse);
+for(let i = 0; i < sentence.length; i++) {
+    // console.log(sentence[i]);
+    reverse = sentence[i] + reverse;
+}
+console.log(reverse);
