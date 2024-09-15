148205230d2ffb62ed39d8f653cba71ffbfb5806
rootnure <nur.diu.2791@gmail.com>
2024-09-15 20:09:51 +0600

    intro to object

---
+++ object-intro.js
@@ -0,0 +1,25 @@
+const age = 39;
+const name = "Mr. Harun";
+const business = "Hotel";
+let isPolice = true;
+const soldItem = ['dal', 'vat', 'vorta'];
+
+const bottle_WRONG = ['transparent', '1000ml', 25, 'water'];
+const bottleColor = 'transparent';
+const bottleVolume = '1000ml';
+const bottlePriceInTK = 25;
+const drinksInTheBottle = 'water';
+
+const bottle = {
+    color: 'transparent',
+    volume: '1000ml',
+    priceInTK: 25,
+    drinksInside: 'water'
+};
+
+const friend = {
+    name: 'Rashed',
+    age: 24,
+    gender: 'male',
+    hobby: ['eating', 'sleeping'],
+}
