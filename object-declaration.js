25a6813e3e232275e83c5113bc06773d4f48181f
rootnure <nur.diu.2791@gmail.com>
2024-09-18 12:22:47 +0600

    multiple ways to declare an object

---
+++ object-declaration.js
@@ -0,0 +1,10 @@
+const pen = {brand: 'Metador', price: 10, model: 'All-Time'};
+console.log(pen);
+
+const pencil = new Object();
+console.log(pencil);
+
+const rubber = Object.create({});
+console.log(rubber);
+
+// object using class
