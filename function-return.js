cc41298547d74d17d8a1382b64599dd2a1f6bc20
rootnure <nur.diu.2791@gmail.com>
2024-09-18 13:13:51 +0600

    Function return

---
+++ function-return.js
@@ -0,0 +1,17 @@
+function tenTimes(number) {
+    const result = number * 10;
+    return result;
+}
+
+function cutHalf(num) {
+    const half = num / 2;
+    return half;
+}
+
+tenTimes(5);
+
+const output = tenTimes(7);
+console.log('output', output);
+
+const ordhek = cutHalf(100);
+console.log('ordhek', ordhek);
