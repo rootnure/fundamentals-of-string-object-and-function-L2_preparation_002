cf58fdece81420060ed90082d11c44415c79b5dc
rootnure <nur.diu.2791@gmail.com>
2024-09-18 13:12:02 +0600

    Function parameter & argument

---
+++ function-arguments.js
@@ -0,0 +1,32 @@
+function doubleIt(number) {
+    const doubled = number * 2;
+    console.log(number, doubled);
+}
+
+// console.log('-------------------');
+// console.log('I will call the function');
+// console.log('-------------------');
+// doubleIt(15);
+// console.log('-------------------');
+// doubleIt(88);
+// console.log('-------------------');
+// doubleIt(79);
+// console.log('-------------------');
+// const newNumber = 44;
+// doubleIt(newNumber);
+// console.log('-------------------');
+// const money = 500;
+// doubleIt(money);
+// console.log('-------------------');
+
+
+function difference(num1, num2) {
+    const diff = num1 - num2;
+    console.log(diff);
+}
+
+difference(91, 20);
+
+const fatherAge = 53;
+const myAge = 26;
+difference(fatherAge, myAge);
