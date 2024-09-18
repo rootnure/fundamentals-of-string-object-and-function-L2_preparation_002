f010b08880816a2cbf7074808da9051a51d983f0
rootnure <nur.diu.2791@gmail.com>
2024-09-18 12:40:28 +0600

    fundamental of function

---
+++ function.js
@@ -0,0 +1,29 @@
+function fanOffKor() {
+    console.log("bosa theke uthe dara");
+    console.log("switch er kace ja");
+    console.log("select the switch of the fan");
+    console.log("click the switch to turn off the fan");
+}
+
+// // function structure
+// function name() {
+//
+// }
+
+function brushTeeth(){
+    console.log("pick up the brash");
+    console.log('add toothpaste');
+    console.log('ghosho and ghosho');
+    console.log('clean the mouth in the end');
+}
+
+// call the function
+console.log('---------------');
+brushTeeth();
+console.log('---------------');
+fanOffKor();
+console.log('---------------');
+brushTeeth();
+console.log('---------------');
+fanOffKor();
+console.log('---------------');
